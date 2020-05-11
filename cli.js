1bf8c7ccff7e6fdaada40528508b9fe5225976d8
Mohamed Dounnani <mohamed.dounnani@weroad.it>
2020-05-11 17:36:00 +0200

    chore(): update cli.js

--- cli.js
+++ cli.js
@@ -7,13 +7,13 @@ const cyan = chalk.cyan;
 module.exports = meow(
   `
 	Usage
-	  ${green(`gen-cli`)} ${cyan(`<schema>`)} ${yellow(`<name>`)}
+	  ${green(`gen-files`)} ${cyan(`<schema>`)} ${yellow(`<name>`)}
 	Commands
 	  ${cyan(`schema`)}    Generate schema
 	  ${cyan(`name`)}      Name of schema to generate
 	Examples
-	${green(`gen-cli`)} ${cyan(`schema`)} ${yellow(`badge`)}
-	${green(`gen-cli`)} ${cyan(`help`)}
+	${green(`gen-files`)} ${cyan(`schema`)} ${yellow(`badge`)}
+	${green(`gen-files`)} ${cyan(`help`)}
 `,
   {
     booleanDefault: undefined,
